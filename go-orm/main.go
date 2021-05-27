fce5936c98c60f882d84ea7b86021484cf67ff73
Sourav <souravgopal25@gmail.com>
2021-05-27 17:37:22 +0530

    CRUD operation completed using GORM

--- go-orm/main.go
+++ go-orm/main.go
@@ -11,8 +11,8 @@ func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", home).Methods("GET")
 	myRouter.HandleFunc("/users", AllUsers).Methods("GET")
-	myRouter.HandleFunc("/user/{name}/{email}", NewUsers).Methods("PUT")
-	myRouter.HandleFunc("user/{name}", DeleteUsers).Methods("DELETE")
+	myRouter.HandleFunc("/user/{name}/{email}", NewUsers).Methods("POST")
+	myRouter.HandleFunc("/user/{name}", DeleteUsers).Methods("DELETE")
 	myRouter.HandleFunc("/user/{name}/{email}", UpdateUser).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8000", myRouter))
 }
--- go-orm/user.go
+++ go-orm/user.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	_ "log"
@@ -13,7 +14,6 @@ type User struct {
 	gorm.Model
 	Name  string
 	Email string
-	Phone string
 }
 
 func InitialMigration() {
@@ -32,9 +32,9 @@ func InitialMigration() {
 	}
 	//var db1 *gorm.DB
 	//Create
-	if err := db.Create(&User{Name: "Sourav Sharma ", Email: "souravgopal25@gmail.com", Phone: "8434682313"}).Error; err != nil {
+	/*if err := db.Create(&User{Name: "Sourav Sharma ", Email: "souravgopal25@gmail.com"}).Error; err != nil {
 		panic(err)
-	}
+	}*/
 
 }
 func connectToDatabse() (*gorm.DB, error) {
@@ -79,13 +79,69 @@ func AllUsers(writter http.ResponseWriter, reader *http.Request) {
 }
 
 func NewUsers(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "New Users Endpoint hit")
+	var db *gorm.DB
+	var err error
+	db, err = connectToDatabse()
+	if err != nil {
+		panic(err)
+	}
+	vars := mux.Vars(request)
+	if err := db.Create(&User{Name: vars["name"], Email: vars["email"]}).Error; err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(writer, "New Users Successfully Created")
 }
 
 func DeleteUsers(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Delete user Endpoint Hit")
+	fmt.Println("Executing Delete Function")
+	var db *gorm.DB
+	var err error
+	db, err = connectToDatabse()
+	if err != nil {
+		panic(err)
+	}
+	vars := mux.Vars(request)
+	name := vars["name"]
+
+	var user []User
+	//soft delete
+	er := db.Where("Name=?", name).Delete(&user).Error
+
+	if er != nil {
+		fmt.Fprintf(writer, er.Error())
+	}
+	//TODO Learn how to handle the ouptut and check whether user has been deleted or not.
+
+	/*	fmt.Println(name)
+		fmt.Println(strconv.Itoa(int(results.RowsAffected)))
+
+		if results.RowsAffected!=0 {
+			db.Delete(&user)
+			fmt.Fprintf(writer, strconv.Itoa(int(results.RowsAffected)) +"User Successfully deleted")
+		}else{
+			fmt.Fprintf(writer, "No User Found in Database")
+		}*/
+
 }
 
 func UpdateUser(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Update User Endpoint Hit")
+	var db *gorm.DB
+	var err error
+	db, err = connectToDatabse()
+	if err != nil {
+		panic(err)
+	}
+	vars := mux.Vars(request)
+	name := vars["name"]
+	email := vars["email"]
+	var user User
+	//soft delete
+	er := db.Where("Name=?", name).Find(&user)
+	if er.Error != nil {
+		panic(er)
+	}
+	user.Email = email
+	db.Save(&user)
+
+	fmt.Fprintf(writer, " User Updated Success")
 }
