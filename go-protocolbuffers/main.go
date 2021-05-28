3b26ebe4bd2f53d87e0fb618f9677eea4406da26
Sourav <souravgopal25@gmail.com>
2021-05-28 10:50:06 +0530

    Protocol Buffer completed

--- go-protocolbuffers/main.go
+++ go-protocolbuffers/main.go
@@ -1,5 +1,34 @@
 package main
 
+//https://developers.google.com/protocol-buffers/docs/reference/go-generated#package
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+)
+
 func main() {
+	fmt.Println("Hello World")
+
+	stundetObject := &Student{
+		Name:  "Sourav Sharma",
+		Email: "souravgopal25@gmail.com",
+		Phone: "+917979970460",
+		Address: &Address{
+			State:   "Jharkhand",
+			City:    "Jamshedpur",
+			Country: "India",
+		},
+	}
+	data, err := proto.Marshal(stundetObject)
+	if err != nil {
+		fmt.Println("Marshaling Error ", err)
+	}
+	fmt.Println(data)
+	stundetObjectDecoded := &Student{}
+	err = proto.Unmarshal(data, stundetObjectDecoded)
+	if err != nil {
+		fmt.Println("Unmarshalling error", err)
+	}
+	fmt.Println(stundetObjectDecoded)
 
 }
